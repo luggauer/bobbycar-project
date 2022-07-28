247ff078432a1bf2fcbf2b020ce243957ce54a71
LayerDE <the_gamer@gmx.net>
2022-07-28 02:54:17 +0200

    removed sbus

--- components/sbus2/src/SBUS_usart.h
+++ components/sbus2/src/SBUS_usart.h
@@ -12,6 +12,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+#pragma once
+
 #include <Arduino.h>
 
 #define NUMBER_OF_CHANNELS 18
